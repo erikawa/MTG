3b53ca3332495eb2af367716aee237e4d93c1fc4
Erick Kawauche <kawauche2@gmail.com>
2016-12-01 21:59:22 -0200

    Adicionado filtro para a busca

--- assets/js/controllers/mtg-search-controller.js
+++ assets/js/controllers/mtg-search-controller.js
@@ -1,6 +1,7 @@
 angular.module('mtg').controller('MtgSearchController', function($scope, $http) {
 
 	$scope.cards = {};
+	$scope.filtro = '';
 
 	var response = $http.get('https://api.magicthegathering.io/v1/cards');
 	response.then(function(cardInfo){
