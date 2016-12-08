0b2273b0b49fd6c2cdb314b84f77ae559294d071
erick kawauche <kawauche2@gmail.com>
2016-12-08 10:28:16 -0200

    Removido dependência bodyParser

--- assets/js/controllers/mtg-search-controller.js
+++ assets/js/controllers/mtg-search-controller.js
@@ -9,7 +9,7 @@ angular.module('mtg').controller('MtgSearchController', function($scope, $http) 
 		if($scope.cardName) {
 			var response = $http.get('https://api.magicthegathering.io/v1/cards?name=' + $scope.cardName);
 			response.then(function(cardInfo){
-				$scope.card = cardInfo.data;
+				$scope.card = cardInfo.data.cards;
 			}).catch(function(error) {
 				console.log(error);
 			});
